303c668874e017f96932c490448af239803bcb8f
Yhoan Andres Galeano Urrea <yhoangaleano@gmail.com>
2018-05-11 19:16:45 -0500

    LED

--- index.js
+++ index.js
@@ -1,12 +1,68 @@
 'use strict'
 
-var app = require('./app');
+// var app = require('./app');
 
-var port = process.env.port || 3977;
-var host = process.env.host || '127.0.0.1';
+// var port = process.env.port || 3977;
+// var host = process.env.host || '127.0.0.1';
 
-var server = app.listen(port, host, function() {
+// var server = app.listen(port, host, function() {
 
-    console.log("Servidor del API Restful corriendo por http://localhost:3977");
+//     console.log("Servidor del API Restful corriendo por http://localhost:3977");
 
+// });
+
+var Raspi = require('raspi-io');
+var five = require('johnny-five');
+var board = new five.Board({
+  io: new Raspi()
+});
+
+board.on("ready", function() { // Once the computer is connected to the Arduino
+    // Save convenient references to the LED pin and an analog pin
+    var LEDpin = new five.Pin(18);
+    var analogPin = new five.Pin('A0');
+
+    var express = require('express'); // Load the library we'll use to set up a basic webserver
+    var app = express(); // And start up that server
+
+    app.get('/', function(req, res) { // what happens when we go to `/`
+        res.send("Hello from `server.js`!"); // Just send some text
+    });
+
+    app.get('/hello', function(req, res) { // what ha1ppens when we go to `/hello`
+        res.sendFile('hello.html', { root: '.' }); // Send back the file `hello.html` located in the current directory (`root`)
+    });
+
+    app.get('/:pin/state', function(req, res) { // what happens when someone goes to `/#/state`, where # is any number
+        console.log("Someone asked for the state of pin", req.params.pin + "…");
+        var pins = {
+            'analog': analogPin,
+            'led': LEDpin
+        };
+        if (pins.hasOwnProperty(req.params.pin)) { // If our pins dictionary knows about the pin name requested
+            pins[req.params.pin].query(function(state) { // Look up the pin object associated with the pin name and query it
+                res.send(state); // sending back whatever the state we get is
+            });
+        }
+        else {
+            var errorMessage = "Sorry, you asked for the state of pin `" + req.params.pin + '`, ' + "but I haven't been told about that pin yet.";
+            res.send(errorMessage);
+        }
+    });
+
+    app.get('/led/off', function(req, res) { // what happens when someone goes to `/led/off`
+        console.log("Someone told me to turn the led off…");
+        LEDpin.low(); // Set the pin referred to by the `LEDpin` variable 'low' (off)
+        res.send("Now the LED for pin 13 should be off."); // And tell the user that it should be off in the webpage
+    });
+
+    app.get('/led/on', function(req, res) { // what happens when someone goes to `/led/off`
+        console.log("Someone told me to turn the led on…");
+        LEDpin.high(); // Set the pin referred to by the `LEDpin` variable 'high' (on)
+        res.send("Now the LED for pin 13 should be on.") // And tell the user that it should be off in the webpage
+    });
+
+    app.listen(3000, function() { // Actually turn the server on
+        console.log("Server's up at http://localhost:3000!");
+    });
 });
